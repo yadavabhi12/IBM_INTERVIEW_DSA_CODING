b6b41e63bc72f1ce57a738ec17e60051c51b4231
Your Name <yadabhishek61@gmail.com>
2026-06-30 21:19:15 +0530

    leet code 543 some change

--- Tree/543-Diameter-of-Binary-Tree.py
+++ Tree/543-Diameter-of-Binary-Tree.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+#  method
 
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
