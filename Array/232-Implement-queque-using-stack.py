b6d775cb4eacbed24c1c40b539d2357e12536559
Your Name <yadabhishek61@gmail.com>
2026-07-07 23:18:59 +0530

    232 implement queque using stack

---
+++ Array/232-Implement-queque-using-stack.py
@@ -0,0 +1,30 @@
+class MyQueue:
+
+    def __init__(self):
+        self.l=[]
+        
+
+    def push(self, x: int) -> None:
+        self.l.append(x)
+        
+
+    def pop(self) -> int:
+        return self.l.pop(0)
+        
+
+    def peek(self) -> int:
+        return self.l[0]
+        
+        
+
+    def empty(self) -> bool:
+        return True if len(self.l)==0 else False
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
