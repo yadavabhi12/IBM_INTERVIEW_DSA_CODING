d8d9e8216a7d793238e49329e3f48d03ee977bf1
Your Name <yadabhishek61@gmail.com>
2026-07-12 23:01:21 +0530

    1379 find corresponding node

---
+++ Tree/1379-Find-a-Corresponding-Node-of-a-Binary.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def getTargetCopy(self, original, cloned, target):
+        """
+        :type original: TreeNode
+        :type cloned: TreeNode
+        :type target: TreeNode
+        :rtype: TreeNode
+        """
+
+        if original is None:
+            return None
+
+        if original == target:
+            return cloned
+
+        left = self.getTargetCopy(original.left, cloned.left, target)
+        if left:
+            return left
+
+        return self.getTargetCopy(original.right, cloned.right, target)
